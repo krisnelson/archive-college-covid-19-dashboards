e339a99ff8911ca6cc7665a89451496fab2b33ae
Kristopher Nelson <krisnelson@gmail.com>
2022-01-13 12:06:55 -0800

    Second run

--- capture-dashboards.sh
+++ capture-dashboards.sh
@@ -11,8 +11,8 @@ while read url; do
     --output "./captures/$(echo "$url" \
     | sed -e 's/[^A-Za-z0-9._-]/-/g').jpeg" \
     --overwrite \
-    --full-page --width 1440 --scale-factor 1 \
-    --type jpeg --quality 0.8 \
-    --delay 10 --timeout=600
+    --full-page --width 1600 \
+    --type jpeg --quality 0.4 \
+    --delay 15 --timeout=600
 
 done < "$filename"
